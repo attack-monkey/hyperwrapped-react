d51ad34bd394431e3a1a26690926feca82f508e1
Ben Clark <bclark.2111@gmail.com>
2018-11-22 15:48:06 +1030

    add polyfill loader

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
-import { app } from "hyperwrap";
+import { app } from "./hyperwrap";
 import { initialState } from "./src/state/state";
 import { View } from "./src/components/view/view.component";
+import { loadPolyfills } from "./utils/polyfills/load-polyfills";
 
+loadPolyfills();
 app(initialState, View, document.getElementById('app'));
